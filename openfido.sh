ceb58365965b91238af2d86d90e85422cfc8c3bd
Peuan Thinsan <peuanthinsan@berkeley.edu>
2020-09-15 10:32:58 -0700

    update script

--- openfido.sh
+++ openfido.sh
@@ -6,27 +6,31 @@ set -e
 echo "OPENFIDO_INPUT = $OPENFIDO_INPUT"
 echo "OPENFIDO_OUTPUT = $OPENFIDO_OUTPUT"
 
-if ls -1 $OPENFIDO_INPUT/*.glm; then # TODO: redirect input of ls elsewhere
-  input_glm=`ls -1 $OPENFIDO_INPUT/*.glm | head -n 1`
-  echo "Input GLM: $input_glm"
-
-  if ls $OPENFIDO_INPUT/*.tmy3 | grep -q .tmy3; then
-    echo "Running GridLabD"
-
-    # Run GridLabD command
-    python3 -u run_gridlabd_main.py \
-    -i $input_glm \
-    -o "${input_glm%.*}_post_run.json"
-  else
-    echo "Input .tmy3 file not found"
-    exit 1
-  fi
-else
+if ! ls -1 $OPENFIDO_INPUT/*.glm; then
   echo "Input .glm file not found"
   exit 1
 fi
 
-mv *.csv $OPENFIDO_OUTPUT                             # will error out with exit code 1
-mv "${input_glm%.*}_post_run.json" $OPENFIDO_OUTPUT
+if ! ls -1 $OPENFIDO_INPUT/*.tmy3; then
+  echo "Input .tmy3 file not found"
+  exit 1
+fi
+
+path_to_tmp_dir=tmp
+
+echo "Copying input files to working directory"
+cp -r $OPENFIDO_INPUT/* .
+
+input_glm=`ls -1 $OPENFIDO_INPUT/*.glm | sed 's#.*/##'`
+echo "Input GLM: $input_glm"
+
+echo "Running GridLabD"
+
+python3 -u run_gridlabd_main.py \
+-i $input_glm \
+-o ${input_glm%.*}_post_run.json
+
+mv *.json $OPENFIDO_OUTPUT
+mv *.csv $OPENFIDO_OUTPUT
 mv gridlabd.* $OPENFIDO_OUTPUT
 mv *.txt $OPENFIDO_OUTPUT
